bda8b766163ced9d8ea3cef0e007ac29ec035177
SumonPB <byroncondolo99@gmail.com>
2025-11-21 18:18:25 -0500

    Comit

--- src/App.vue
+++ src/App.vue
@@ -11,7 +11,7 @@ import HelloWorld from './components/HelloWorld.vue'
       <img src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
     </a>
   </div>
-  <HelloWorld msg="Hola Mundo Byron Condolo Narvaez" />
+  <HelloWorld msg="Hola Mundo Byron Condolo Narvaez Sumon" />
 </template>
 
 <style scoped>
